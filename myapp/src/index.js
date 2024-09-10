1d3cd6193045ac0567f73453e3efb183ac4b2292
Santiago Tapias <ds.tapias@uniandes.edu.co>
2024-09-10 10:17:22 -0500

    Ejercicio en clase de react terminado

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,13 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Post from './components/Post'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const component = <h1>Hola mundo</h1>;
+//root.render(component);
+//root.render(<App />);
+/*root.render(
+  <Post author="John Doe" content="This is the post content" likes={20} />
+ );*/
+ /*root.render(
+  <div>
+    <Post author="John Doe" content="This is the post content" likes={20} />
+    <Post author="Anne Hill" content="I like React" likes={3} />
+    <Post author="Laia Martins" content="I love JSX" likes={0} />
+  </div>
+ );*/
+ root.render(
+  <Container className="mt-3">
+    <Row>
+      <Post author="John Doe" content="This is the post content" likes={20} />
+      <Post author="Anne Hill" content="I like React" likes={3} />
+      <Post author="Laia Martins" content="I love JSX" likes={0} />
+    </Row>
+  </Container>
+ );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
